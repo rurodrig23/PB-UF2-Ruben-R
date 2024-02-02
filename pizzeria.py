8f1d87cde0387f6cb8853f0003a5475910e10b43
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-02-02 09:17:47 +0100

    he borrado todo el codigo y solo tengo 1 funcion:)

--- pizzeria.py
+++ pizzeria.py
@@ -1,6 +1,12 @@
+pizzeria = []
+estoc_ingredients = {"tomate": 100, "formatge": 50, "pepperoni": 20}
+ingredients_extra = {"bacon": 100, "queso de cabra": 50, "carne": 20}
 
+def agregar_comanda(pizzeria, estoc_ingredients):
 
-    if opcion == "1":
+ 
+
+if opcion == "1":
         agregar_comanda(pizzeria)
 
     elif opcion == "2":
