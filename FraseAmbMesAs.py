cd542c7de8ce740e287913a629efcd75ad41eeb5
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-02-16 09:51:50 +0100

    metemos en el bucle todo para que lo coja y printeamos

--- FraseAmbMesAs.py
+++ FraseAmbMesAs.py
@@ -6,16 +6,21 @@ def contar_letras_a(frase):
     return count
 
 def frase_amb_mes_as():
-    max_as = 0
-    frase_mes_as = ""
+    while True:
+        max_as = 0
+        frase_mes_as = ""
+        frase = input("Escribe una frase:\n> ")
+        
 
-while True:
-    frase = input("Escribe una frase:\n> ")
-
-    if frase.lower() == "fi":
+        if frase.lower() == "fi":
             break
    
-num_as = contar_letras_a(frase)
-if num_as > max_as:
+        num_as = contar_letras_a(frase)
+        if num_as > max_as:
             max_as = num_as
             frase_mes_as = frase
+
+        print(f'La frase amb més \'a\' és: "{frase_mes_as}"')
+        print(f'Té {max_as} \'a\'.')
+    
+
