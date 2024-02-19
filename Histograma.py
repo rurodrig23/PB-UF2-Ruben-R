88b0f102318bbac304281a2e2746f8c564721692
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-02-19 12:59:44 +0100

    mejor hacemos un append para que lo guarde en tiradas

--- Histograma.py
+++ Histograma.py
@@ -2,7 +2,9 @@
 def generar_histograma():
     tirades = [0] * 11
 
-for i in range(1,7):
-    for j in range(1,7):
-        suma_dados = i+j
-        tirades[suma_dados] += 1
+    for i in range(1,7):
+        for j in range(1,7):
+            suma_dados = i+j
+            suma_dados.append(tirades)
+
+        return tirades
