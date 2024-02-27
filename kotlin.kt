86b57646749b656065999165d8b6f5f7a10fed1a
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-02-27 10:18:43 +0100

    empezamos

---
+++ kotlin.kt
@@ -0,0 +1,27 @@
+
+
+
+val n1= readline("Dime el primer numero")
+val n2= readline("Dime el segundo numero")
+
+
+fun main() {
+
+}
+
+fun sumar() {
+    
+}
+
+fun restar() {
+    
+}
+
+fun multiplicar() {
+    
+}
+
+
+fun dividir() {
+    
+}
