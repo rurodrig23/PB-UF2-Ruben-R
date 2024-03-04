8190fadc2c854ed31515d2cd4a616ff53bbeb00f
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-03-04 13:00:57 +0100

    2 fun principales

--- agencia.kt
+++ agencia.kt
@@ -1,1 +1,13 @@
+fun main() {
+    val travelAgency = TravelAgency()
 
+    println("Benvingut a l'Agència de Viatges")
+}
+
+
+fun afegir viatge() {
+    val viatge = Viatge(destinacio, durada, preu)
+
+    llistaViatges.add(viatge)
+
+}
