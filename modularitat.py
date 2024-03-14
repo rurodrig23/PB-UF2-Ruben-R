85b51656c5ca5fe744413b768c40737d129553a6
«rurodrig23» <usuari@rubennrp37@gmail.com>
2024-03-14 09:22:26 +0100

    codigo para empezar en kotlin

--- modularitat.py
+++ modularitat.py
@@ -1,31 +1,26 @@
+class Coordenada:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def moure_dreta(self):
+        self.x += 1
+    
+    def moure_esquerra(self):
+        self.x -= 1
+    
+    def moure_amunt(self):
+        self.y += 1
+    
+    def moure_avall(self):
+        self.y -= 1
 
 # Coordenada inicial
-coordenada = (0, 0)
+coordenada = Coordenada(0, 0)
 
-# Programa principal
-def moure_dreta(coordenada):
-    x, y = coordenada
-    nova_coordenada = (x + 1, y)
-    return nova_coordenada
+# Ejecutar movimientos
+coordenada.moure_dreta()
+print(f"Nueva coordenada después de mover a la derecha: ({coordenada.x}, {coordenada.y})")
 
-def moure_esquerra(coordenada):
-    x, y = coordenada
-    nova_coordenada = (x - 1, y)
-    return nova_coordenada
-
-def moure_amunt(coordenada):
-    x, y = coordenada
-    nova_coordenada = (x, y + 1)
-    return nova_coordenada
-
-def moure_avall(coordenada):
-    x, y = coordenada
-    nova_coordenada = (x, y - 1)
-    return nova_coordenada
-
-# Executar moviments
-coordenada = moure_dreta(coordenada)
-print(f"Nova coordenada després de moure a la dreta: {coordenada}")
-
-coordenada = moure_amunt(coordenada)
-print(f"Nova coordenada després de moure amunt: {coordenada}")
+coordenada.moure_amunt()
+print(f"Nueva coordenada después de mover arriba: ({coordenada.x}, {coordenada.y})")
